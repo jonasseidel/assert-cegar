2c46bc1fad53b53c95983a0c0606ce5eb4ee6be9
Jonas Seidel <seideljonas@web.de>
2026-04-08 10:42:22 +0200

    Fixed issue: Missing mapping from chars to small indeces for the minimal example transition table. Replaced -1 index for absorbing state

--- tests/DFA_tests.cpp
+++ tests/DFA_tests.cpp
@@ -3,15 +3,16 @@
 #include <cassert>
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 #include "../src/lexer/DFA.h"
 
 static int failures = 0;
 
 std::vector<std::vector<int>> tt = {
-        {0, 1, -1},
-        {0, -1, 2},
-        {-1, -1, -1},
-        {-1, -1, -1},
+        {0, 1, 3},
+        {0, 3, 2},
+        {3, 3, 3},
+        {3, 3, 3},
     };
 
 std::vector<std::optional<Token>> as = {
@@ -21,6 +22,22 @@ std::vector<std::optional<Token>> as = {
     std::nullopt,
 };
 
+// For testing miniimal DFA examples, we add a mapping from chars to indices in the transition table.
+// For the actual lexer dfa, we use the ASCII value directly.
+std::unordered_map<char, int> charToIndex = {
+    {'a', 0},
+    {'b', 1},
+    {'c', 2},
+};
+
+std::string wordToIndices(std::string_view word) {
+    std::string indices;
+    for (char c : word) {
+        indices += static_cast<char>(charToIndex[c]);
+    }
+    return indices;
+}
+
 
 void test_accepts_valid_input() {
     // Test just to words of a simple DFA
@@ -28,11 +45,11 @@ void test_accepts_valid_input() {
     // generating many testcases from the regex of each token type.
 
     DFA dfa(tt, as);
-    int state = dfa.solveWordProblem("abaaabaaabc");
+    int state = dfa.solveWordProblem(wordToIndices("abaaabaaabc"));
     assert(dfa.isAccepting() == true && dfa.getAcceptedToken(state).type == TokenType::WHILE); // identifier
 
     dfa.reset();
-    state = dfa.solveWordProblem("aaaaaa");
+    state = dfa.solveWordProblem(wordToIndices("aaaaaa"));
     assert(dfa.isAccepting() == true && dfa.getAcceptedToken(state).type == TokenType::IDENTIFIER); // identifier
     
     std::cout << "PASS test_accepts_valid_input\n";
@@ -42,22 +59,22 @@ void test_accepts_valid_input() {
 void test_absorbing_state() {
     
     DFA dfa(tt, as);
-    int state = dfa.solveWordProblem("ac");
+    int state = dfa.solveWordProblem(wordToIndices("ac"));
     assert(dfa.isAccepting() == false && dfa.isAbsorbing() == true); // should be in the absorbing state
-    assert(dfa.step('a') == state);
-    assert(dfa.step('b') == state);
-    assert(dfa.step('c') == state);
+    assert(dfa.step(charToIndex['a']) == state);
+    assert(dfa.step(charToIndex['b']) == state);
+    assert(dfa.step(charToIndex['c']) == state);
      //should stay in the absorbing state
     std::cout << "PASS test_absorbing_state\n";
 }
 
 void test_rejects_invalid_input() {
     DFA dfa(tt, as);
-    int state = dfa.solveWordProblem("ab");
+    int state = dfa.solveWordProblem(wordToIndices("ab"));
     assert(dfa.isAccepting() == false);
 
     dfa.reset();
-    state = dfa.solveWordProblem("bbc");
+    state = dfa.solveWordProblem(wordToIndices("bbc"));
     assert(dfa.isAccepting() == false);
 
     std::cout << "PASS test_rejects_invalid_input\n";
