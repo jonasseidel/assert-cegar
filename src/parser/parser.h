0ee969c44fb06a07fd8fea202d20e776c2ffe1d4
Jonas Seidel <seideljonas@web.de>
2026-05-03 19:38:40 +0200

    Added todo notes

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -8,6 +8,10 @@ class Parser{
 std::vector<Token> tokens;
 std::vector<Token>::iterator curr;
 
+std::vector<int> itemsetStack;
+
+//Add syntax tree field
+
 // TODO members for action, goto table for parsing algorithm. EIther
 // we will do LL or LR parsing.
 
@@ -15,4 +19,21 @@ public:
     explicit Parser(const std::vector<Token>& t) : tokens(t), curr(tokens.begin()) {
     
     };
+
+    // implement shift function using table
+
+    //implement get action function
+
+    // implement step function, reading next token input, then depending on next action
+    //call reduce i, shift or accept...
+    // Either directly building syntax tree or remebering sequence of i.
+    // Does parsing_tables misses production information?
+
+    //  implement reduce i
+
+    //implement error function
+
+    //
+
+
 };
